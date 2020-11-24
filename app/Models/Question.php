db68c60f6fe3ac571d373cf121a48b0599ec1476
Guillaume Chantraine <guillaume.chantraine@gmail.com>
2020-11-24 12:50:07 +0100

    Model\Question généré via artisan et ajout d'une méthode pour modifier title et slug en même temps

---
+++ app/Models/Question.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'body'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value) {
+        $this->attributes['title']  = $value;
+        $this->attributes['slug']   = Str::slug($value);
+    }
+}
