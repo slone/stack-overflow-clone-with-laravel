c58224c3988b60e4de0e88a914f8217b3e3477cc
Guillaume Chantraine <guillaume.chantraine@gmail.com>
2020-11-27 01:22:46 +0100

    added markup and text for votes and favorite interfaces. This ends part 1

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,25 +5,48 @@
 	<div class="row justify-content-center">
 		<div class="col-md-8">
 			<div class="card">
-				<div class="card-header">
-					<div class="d-flex align-items-center">
-						<h1>{{ $question->title }}</h1>
-						<div class="ml-auto">
-							<a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">{{ __('Back to all questions')}}</a>
+				<div class="card-body p1">
+					<div class="card-title">
+						<div class="d-flex align-items-center">
+							<h1>{{ $question->title }}</h1>
+							<div class="ml-auto">
+								<a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">{{ __('Back to all questions')}}</a>
+							</div>
 						</div>
 					</div>
-				</div>
 
-				<div class="card-body">
-                    <div class="">{!! $question->body_html !!}</div>
-					<div class="float-right">
-						<span class="text-muted">Asked {{ $question->created_at }}</span>
-						<div class="media mt-2">
-							<a href="{{ $question->user->url }}" class="pr-2">
-								<img src="{{ $question->user->avatar }}" alt="avatar of {{ $question->user->name }}">
+					<hr>
+
+					<div class="media">
+						<div class="d-flex flex-column">
+							<a title="this question is useful" class="vote-up">
+								Vote up
 							</a>
-							<div class="media-body mt-1">
-								<a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+							<span class="votes-count">
+								1224
+							</span>
+							<a title="this question is not useful" class="vote-down off">
+								Vote down
+							</a>
+							<a title="Click to favorite the question - click again to undo" class="favorite">
+								favorite it
+							</a>
+							<span class="favorites-count">
+								54
+							</span>
+						</div>
+						<div class="media-body">
+							<div>{!! $question->body_html !!}</div>
+							<div class="float-right">
+								<span class="text-muted">Asked {{ $question->created_at }}</span>
+								<div class="media mt-2">
+									<a href="{{ $question->user->url }}" class="pr-2">
+										<img src="{{ $question->user->avatar }}" alt="avatar of {{ $question->user->name }}">
+									</a>
+									<div class="media-body mt-1">
+										<a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
