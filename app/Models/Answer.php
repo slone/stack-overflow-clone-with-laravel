2b481fca46c1bed45202c6cab2f84b4908397c0b
Guillaume Chantraine <guillaume.chantraine@gmail.com>
2020-11-27 12:11:32 +0100

    implemented automatic nullification of best_awswer_id using foreign key

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -35,17 +35,13 @@ class Answer extends Model
 
 	public static function boot() {
 		parent::boot();
+		
 		static::created(function($answer) {
 			$answer->question->increment('answers_count');
 		});
 
 		static::deleted(function($answer) {
-			$question = $answer->question;
-			$question->decrement('answers_count');
-			if ($question->best_answer_id === $answer->id) {
-				$question->best_answer_id = NULL;
-				$question->save();
-			}
+			$answer->question->decrement('answers_count');
 		});
 	}
 
