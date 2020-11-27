8f154cc4b66f0c2cdf7ad523ceda5ef752f7242c
Guillaume Chantraine <guillaume.chantraine@gmail.com>
2020-11-27 03:24:15 +0100

    set best_answer_id column in question to NULL when best answer is deleted

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -21,7 +21,7 @@
                                 <a title="this answer is not useful" class="vote-down off">
                                     <i class="fas fa-caret-down fa-2x"></i>
                                 </a>
-                                <a title="Mark as best answer" class="vote-accepted mt-2">
+                                <a title="Mark as best answer" class="{{ $answer->status }} mt-2">
                                     <i class="fas fa-check fa-2x"></i>
                                 </a>
                                 <span class="favorites-count">
